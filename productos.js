4c8d0b97d095c1ac59900e8d8cfbd9d16106fda8
Paula Martinez <martinezmpaula@gmail.com>
2022-05-01 23:05:04 -0300

    ajustes finales

--- productos.js
+++ productos.js
@@ -1,25 +1,25 @@
-class Carrito {
+class Cart {
     constructor(productos) {
         this.productos = productos;
     }
 
     addProducto(producto) {
         let mapped = this.productos.map(element => element.producto);
-        let enCarrito = mapped.find(element => element.id === producto.id);
+        let inCart = mapped.find(element => element.id === producto.id);
 
-            if (!enCarrito) {
-            this.productos.push({ cantidad: 1, producto });
+            if (!inCart) {
+            this.productos.push({ quantity: 1, producto });
             } else {
 
                 let indexed = mapped.map(element => element.id);
                 let index = indexed.indexOf(producto.id);
-                this.productos[index].cantidad += 1;
+                this.productos[index].quantity += 1;
             }
 
         console.log(this.productos);
     }
 
-    guardar() {
-        localStorage.setItem("MI_CARRITO", JSON.stringify(this.productos));
+    saveCart() {
+        localStorage.setItem("MY_CART", JSON.stringify(this.productos));
     }
 }
---
+++ warehouse.js
@@ -0,0 +1,18 @@
+//Creo el array warehouse
+const warehouse = [
+    { id: 1, name: "Puerh", variety: "Rojo", price: 550, category: "Te", stock: 96, photo: "../img/tePuerh.png" },
+    { id: 2, name: "Earl Grey", variety: "Negro", price: 600, category: "Te", stock: 106, photo: "../img/teEarlGrey.png" },
+    { id: 3, name: "Antonieta", variety: "Negro", price: 400, category: "Te", stock: 112, photo: "../img/teAntonieta.png" },
+    { id: 4, name: "Frutos del Bosque", variety: "Negro", price: 400, category: "Te", stock: 99, photo: "../img/teFrutosDelBosque.png" },
+    { id: 5, name: "Chai", variety: "Negro", price: 400, category: "Te", stock: 89, photo: "../img/teChai.png" },
+    { id: 6, name: "Verde Miel", variety: "Verde", price: 400, category: "Te", stock: 68, photo: "../img/teVerdeMiel.png" },
+    { id: 7, name: "Verde Frutal", variety: "Verde", price: 400, category: "Te", stock: 32, photo: "../img/teVerdeFrutal.png" },
+    { id: 8, name: "Verde Citrico", variety: "Verde", price: 400, category: "Te", stock: 65, photo: "../img/teVerdeCitrico.png" },
+    { id: 9, name: "Mix Serrano", variety: "Hierbas", price: 200, category: "Yerba Mate", stock: 65, photo: "../img/yerbaMixSerrano.png" },
+    { id: 10, name: "Frutos Rojos", variety: "Frutos", price: 200, category: "Yerba Mate", stock: 76, photo: "../img/yerbaFrutosRojos.png" },
+]
+
+const categories = [
+    { id: 1, name: "Te" },
+    { id: 2, name: "Yerba Mate" }
+]
